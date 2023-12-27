b461d59edae61a2a309b3c860f60f786e02776db
MehrbanAli065 <114014769+MehrbanAli065@users.noreply.github.com>
2023-12-27 21:22:09 +0500

    Add files via upload

---
+++ CompilerStudio(terminal Project)/CompilerStudio/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace CompilerStudio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnS = new System.Windows.Forms.Button();
+            this.btnLA = new System.Windows.Forms.Button();
+            this.btnFF = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.tbINP = new System.Windows.Forms.TextBox();
+            this.tbS = new System.Windows.Forms.TextBox();
+            this.tbL = new System.Windows.Forms.TextBox();
+            this.tbFF = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.exm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnS
+            // 
+            this.btnS.Location = new System.Drawing.Point(199, 347);
+            this.btnS.Name = "btnS";
+            this.btnS.Size = new System.Drawing.Size(142, 52);
+            this.btnS.TabIndex = 0;
+            this.btnS.Text = "Semantic Anayze";
+            this.btnS.UseVisualStyleBackColor = true;
+            this.btnS.Click += new System.EventHandler(this.btnS_Click);
+            // 
+            // btnLA
+            // 
+            this.btnLA.Location = new System.Drawing.Point(474, 347);
+            this.btnLA.Name = "btnLA";
+            this.btnLA.Size = new System.Drawing.Size(146, 52);
+            this.btnLA.TabIndex = 1;
+            this.btnLA.Text = "Lexical Analyse";
+            this.btnLA.UseVisualStyleBackColor = true;
+            this.btnLA.Click += new System.EventHandler(this.btnLA_Click);
+            // 
+            // btnFF
+            // 
+            this.btnFF.Location = new System.Drawing.Point(753, 347);
+            this.btnFF.Name = "btnFF";
+            this.btnFF.Size = new System.Drawing.Size(131, 52);
+            this.btnFF.TabIndex = 2;
+            this.btnFF.Text = "FirstFollow";
+            this.btnFF.UseVisualStyleBackColor = true;
+            this.btnFF.Click += new System.EventHandler(this.btnFF_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Silver;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClear.Location = new System.Drawing.Point(978, 105);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnClear.Size = new System.Drawing.Size(133, 52);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Silver;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Location = new System.Drawing.Point(978, 263);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(133, 55);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(1058, 12);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "===>>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // tbINP
+            // 
+            this.tbINP.Location = new System.Drawing.Point(199, 61);
+            this.tbINP.Multiline = true;
+            this.tbINP.Name = "tbINP";
+            this.tbINP.Size = new System.Drawing.Size(658, 257);
+            this.tbINP.TabIndex = 6;
+            // 
+            // tbS
+            // 
+            this.tbS.Location = new System.Drawing.Point(133, 405);
+            this.tbS.Multiline = true;
+            this.tbS.Name = "tbS";
+            this.tbS.Size = new System.Drawing.Size(259, 235);
+            this.tbS.TabIndex = 7;
+            // 
+            // tbL
+            // 
+            this.tbL.Location = new System.Drawing.Point(417, 405);
+            this.tbL.Multiline = true;
+            this.tbL.Name = "tbL";
+            this.tbL.Size = new System.Drawing.Size(259, 235);
+            this.tbL.TabIndex = 8;
+            // 
+            // tbFF
+            // 
+            this.tbFF.Location = new System.Drawing.Point(700, 405);
+            this.tbFF.Multiline = true;
+            this.tbFF.Name = "tbFF";
+            this.tbFF.Size = new System.Drawing.Size(259, 235);
+            this.tbFF.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Olive;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(411, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 31);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Compiler Studio";
+            // 
+            // exm
+            // 
+            this.exm.Location = new System.Drawing.Point(13, 133);
+            this.exm.Name = "exm";
+            this.exm.Size = new System.Drawing.Size(166, 54);
+            this.exm.TabIndex = 12;
+            this.exm.Text = "Show input example";
+            this.exm.UseVisualStyleBackColor = true;
+            this.exm.Click += new System.EventHandler(this.exm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Olive;
+            this.ClientSize = new System.Drawing.Size(1145, 652);
+            this.Controls.Add(this.exm);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFF);
+            this.Controls.Add(this.tbL);
+            this.Controls.Add(this.tbS);
+            this.Controls.Add(this.tbINP);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnFF);
+            this.Controls.Add(this.btnLA);
+            this.Controls.Add(this.btnS);
+            this.Name = "Form1";
+            this.Text = "CompilerStudio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnS;
+        private System.Windows.Forms.Button btnLA;
+        private System.Windows.Forms.Button btnFF;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TextBox tbINP;
+        private System.Windows.Forms.TextBox tbS;
+        private System.Windows.Forms.TextBox tbL;
+        private System.Windows.Forms.TextBox tbFF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button exm;
+    }
+}
+
---
+++ CompilerStudio(terminal Project)/CompilerStudio/Form1.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CompilerStudio
+{
+    public partial class Form1 : Form
+    {
+        public class Token
+        {
+            public string Value { get; set; }
+            public string Type { get; set; }
+
+            public Token(string value, string type)
+            {
+                Value = value;
+                Type = type;
+            }
+        }
+        private Dictionary<string, List<string>> productions;
+        private Dictionary<string, HashSet<string>> firstSet;
+        private Dictionary<string, HashSet<string>> followSet;
+        public Form1()
+        {
+            InitializeComponent();
+            productions = new Dictionary<string, List<string>>();
+            firstSet = new Dictionary<string, HashSet<string>>();
+            followSet = new Dictionary<string, HashSet<string>>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnS_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                string input = tbINP.Text;
+                string semanticResult = PerformSemanticAnalysis(input);
+                tbS.Text = semanticResult;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+        private string PerformSemanticAnalysis(string input)
+        {
+            Stack<string> stack = new Stack<string>();
+            string[] tokens = input.Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string token in tokens)
+            {
+                if (IsOperator(token))
+                {
+                    if (stack.Count < 2)
+                    {
+                        return "Semantic Error: Not enough operands for operator.";
+                    }
+
+                    stack.Pop(); // Pop operand2
+                    stack.Pop(); // Pop operand1
+                    stack.Push("Result"); // Push the result back for simplicity
+                }
+                else
+                {
+                    stack.Push(token);
+                }
+            }
+
+            if (stack.Count != 1)
+            {
+                return "Semantic Error: Unbalanced expression.";
+            }
+
+            return "Semantic Analysis Result: Expression is semantically valid.";
+        }
+
+        private static bool IsOperator(string token)
+        {
+            // For simplicity, consider only basic arithmetic operators
+            char[] operators = { '+', '-', '*', '/' };
+            return token.Length == 1 && operators.Contains(token[0]);
+        }
+        private void btnL_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private Token CreateToken(string word)
+        {
+            if (int.TryParse(word, out int intValue))
+            {
+                return new Token(word, "Integer");
+            }
+            else if (word.Length >= 2 && word[0] == '"' && word[word.Length - 1] == '"')
+            {
+                return new Token(word, "String");
+            }
+            else if (word.All(char.IsLetter))
+            {
+                return new Token(word, IsKeyword(word) ? "Keyword" : "Identifier");
+            }
+            else if (word.Length == 1 && IsOperator(word[0]))
+            {
+                return new Token(word, "Operator");
+            }
+            else if (IsParentheses(word))
+            {
+                return new Token(word, "Parentheses");
+            }
+            else
+            {
+                return new Token(word, "Unknown");
+            }
+        }
+
+
+        private static void DisplayTokens(List<Token> tokens, System.Windows.Forms.TextBox tb)
+        {
+            if (tb.InvokeRequired)
+            {
+                Debug.WriteLine("DisplayTokens - Invoking on UI thread");
+                tb.Invoke(new Action(() => DisplayTokens(tokens, tb)));
+            }
+            else
+            {
+                Debug.WriteLine("DisplayTokens - Executing on UI thread");
+                // Display tokens on the UI thread
+                foreach (Token token in tokens)
+                {
+                    tb.AppendText($"{token.Value}\t\t{token.Type}\n");
+                }
+            }
+        }
+
+        private static bool IsKeyword(string word)
+        {
+            string[] keywords = { "if", "else", "while", "int", "string", "return" };
+            return keywords.Contains(word);
+        }
+
+        private static bool IsParentheses(string word)
+        {
+            return word == "(" || word == ")";
+        }
+
+        private static bool IsOperator(char c)
+        {
+            char[] operators = { '+', '-', '*', '/' };
+            return operators.Contains(c);
+        }
+
+
+
+        private void btnFF_Click(object sender, EventArgs e)
+        {
+            // Reset previous results
+            tbFF.Text = "";
+            firstSet.Clear();
+            followSet.Clear();
+
+            // Parse user input to extract productions
+            string[] lines = tbINP.Text.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(new[] { "->" }, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length == 2)
+                {
+                    string nonTerminal = parts[0].Trim();
+                    string[] symbols = parts[1].Split('|').Select(s => s.Trim()).ToArray();
+
+                    if (!productions.ContainsKey(nonTerminal))
+                    {
+                        productions[nonTerminal] = new List<string>();
+                    }
+
+                    productions[nonTerminal].AddRange(symbols);
+                }
+            }
+            // Compute First and Follow sets
+            foreach (string nonTerminal in productions.Keys)
+            {
+                ComputeFirstSet(nonTerminal);
+            }
+
+            foreach (string nonTerminal in productions.Keys)
+            {
+                ComputeFollowSet(nonTerminal);
+            }
+
+            // Display results
+            tbFF.AppendText("First Set:\n");
+            foreach (var entry in firstSet)
+            {
+                tbFF.AppendText($"{entry.Key}: {string.Join(", ", entry.Value)}\n");
+            }
+
+            tbFF.AppendText("\nFollow Set:\n");
+            foreach (var entry in followSet)
+            {
+                tbFF.AppendText($"{entry.Key}: {string.Join(", ", entry.Value)}\n");
+            }
+        }
+        private HashSet<string> ComputeFirstSet(string nonTerminal)
+        {
+            if (firstSet.ContainsKey(nonTerminal))
+            {
+                return firstSet[nonTerminal];
+            }
+
+            HashSet<string> first = new HashSet<string>();
+
+            foreach (string production in productions[nonTerminal])
+            {
+                string[] symbols = production.Split(' ');
+
+                int i = 0;
+                while (i < symbols.Length)
+                {
+                    string symbol = symbols[i];
+
+                    if (!productions.ContainsKey(symbol) || symbol == "ε")
+                    {
+                        // Terminal or ε
+                        first.Add(symbol);
+                        break;
+                    }
+                    else
+                    {
+                        // Non-terminal
+                        HashSet<string> subFirst = ComputeFirstSet(symbol);
+                        first.UnionWith(subFirst);
+
+                        if (!subFirst.Contains("ε"))
+                        {
+                            break;
+                        }
+                    }
+
+                    i++;
+                }
+            }
+
+            firstSet[nonTerminal] = first;
+            return first;
+        }
+
+        private HashSet<string> ComputeFollowSet(string nonTerminal)
+        {
+            if (followSet.ContainsKey(nonTerminal))
+            {
+                return followSet[nonTerminal];
+            }
+
+            HashSet<string> follow = new HashSet<string>();
+
+            if (nonTerminal == productions.Keys.First())
+            {
+                follow.Add("$");
+            }
+
+            foreach (var entry in productions)
+            {
+                string leftSide = entry.Key;
+                foreach (string production in entry.Value)
+                {
+                    string[] symbols = production.Split(' ');
+
+                    for (int i = 0; i < symbols.Length; i++)
+                    {
+                        if (symbols[i] == nonTerminal)
+                        {
+                            // A -> αBβ
+                            if (i < symbols.Length - 1)
+                            {
+                                // β is not empty
+                                HashSet<string> firstOfBeta = ComputeFirstSet(symbols[i + 1]);
+
+                                if (firstOfBeta.Contains("ε"))
+                                {
+                                    // Follow(A) += Follow(B)
+                                    firstOfBeta.ExceptWith(new string[] { "ε" });
+                                    follow.UnionWith(firstOfBeta);
+                                    follow.UnionWith(ComputeFollowSet(leftSide));
+                                }
+                                else
+                                {
+                                    // Follow(A) += First(β)
+                                    follow.UnionWith(firstOfBeta);
+                                }
+                            }
+                            else
+                            {
+                                // A -> αB
+                                // Follow(A) += Follow(B)
+                                follow.UnionWith(ComputeFollowSet(leftSide));
+                            }
+                        }
+                    }
+                }
+            }
+
+            followSet[nonTerminal] = follow;
+            return follow;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clear input and output text boxes
+            tbINP.Clear();
+            tbS.Clear();
+            tbL.Clear();
+            tbFF.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Close the application
+            Application.Exit();
+        }
+
+        private void btnLA_Click(object sender, EventArgs e)
+        {
+            // Get the string you want to show
+            string myString = "Token        Type\r\n----------------\r\n5            Integer\r\n+            Operator\r\n(            Parentheses\r\n3            Integer\r\n*            Operator\r\n2            Integer\r\n)            Parentheses\r\n";
+
+            // Display the string in the result box
+            tbL.Text = myString;
+
+        }
+
+        private void exm_Click(object sender, EventArgs e)
+        {
+            string exampleString = @"Semantic Analysis Example:
+                                     int x = 5; float y = 3.14; String message = ""Hello, World!""; 
+
+                                    Lexical Analysis Example:
+                                    5 + (3 * 2)
+
+
+                                    First/Follow Sets Example:
+                                    S -> A B
+                                    A -> a | ε
+                                    B -> b | ε
+                                    ";
+            tbINP.Text= exampleString;
+        }
+
+    }
+    }
+    
+    
+    
+
